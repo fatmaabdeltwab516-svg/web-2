3f2c9bb5c6a6066de02f0cdec9a5f9fb5e81e903
fatmaabdeltwab516-svg <fatmaabdeltwab516@gmail.com>
2026-09-12 20:34:53 +0300

    add page aboutus and subscribe and edit page  index

---
+++ subscribe.js
@@ -0,0 +1,11 @@
+
+
+let model = document.querySelector('#model');
+let Createmodel = document.querySelector('.Createmodel');
+
+const createblan=()=>{
+    model.style. display='flex';
+}
+const Checkout=()=>{
+     model.style. display='none';
+}
